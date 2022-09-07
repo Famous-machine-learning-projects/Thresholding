1b0758898a203825510a82e27f0535218147b930
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-09-07 13:57:05 +0430

    Update code.py

--- Adaptive Thresholding/code.py
+++ Adaptive Thresholding/code.py
@@ -8,8 +8,7 @@ th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
             cv.THRESH_BINARY,11,2)
 th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv.THRESH_BINARY,11,2)
-titles = ['Original Image', 'Global Thresholding (v = 127)',
-            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+titles = ['Original Image', 'Global Thresholding (v = 127)', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
 images = [img, th1, th2, th3]
 for i in range(4):
     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
