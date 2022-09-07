427d646bdc20311588be4e6ea3b5fcd25132dc74
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-09-07 13:57:32 +0430

    Update code.py

--- Simple Thresholding/code.py
+++ Simple Thresholding/code.py
@@ -9,6 +9,7 @@ ret,thresh4 = cv.threshold(img,127,255,cv.THRESH_TOZERO)
 ret,thresh5 = cv.threshold(img,127,255,cv.THRESH_TOZERO_INV)
 titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
 images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
 for i in range(6):
     plt.subplot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
     plt.title(titles[i])
